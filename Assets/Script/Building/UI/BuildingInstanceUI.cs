de1b7f9c93f509a3260dfd1baff6adeb25e2245d
Sumit Panwar <93528833+e-sillix@users.noreply.github.com>
2025-02-02 10:42:51 +0530

    Checkpoint

--- Assets/Script/Building/UI/BuildingInstanceUI.cs
+++ Assets/Script/Building/UI/BuildingInstanceUI.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using TMPro;
-using UnityEditor.VersionControl;
+
 
 public class BuildingInstanceUI : MonoBehaviour
 {//this is a manager attached to BuildingUIManager ,for showing data
---
+++ Assets/Script/Enemy/Bosses/Boss.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] private int troopLevel, agressiveLevel,armySize;
+    [SerializeField] private int [] expertiseTroopType,noOfArmy;
+    [SerializeField] private string bossName;
+    [SerializeField] private string[] Rewards, Treasure;
+    //commander maybe.
+}
