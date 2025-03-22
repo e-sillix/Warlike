90508adf214e403ec191ce709f4373760f125077
Sumit Panwar <93528833+e-sillix@users.noreply.github.com>
2025-03-22 16:50:50 +0530

    currency persistence

--- Assets/Script/Currency/CurrencyManager.cs
+++ Assets/Script/Currency/CurrencyManager.cs
@@ -1,85 +1,86 @@
-using UnityEngine.UI; 
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
 
 public class CurrencyManager : MonoBehaviour
 {
-    [SerializeField]private int deltaWood ;
-    [SerializeField]private int deltaGrain ;
-    [SerializeField]private int deltaStone ;
-    [SerializeField]private TextMeshProUGUI woodsCounter;    
-    [SerializeField]private TextMeshProUGUI grainCounter;    
-    [SerializeField]private TextMeshProUGUI stoneCounter;    
-    // [SerializeField] private int woodCurrentCurrency=0;
+    [SerializeField] private int deltaWood;
+    [SerializeField] private int deltaGrain;
+    [SerializeField] private int deltaStone;
+    [SerializeField] private TextMeshProUGUI woodsCounter;
+    [SerializeField] private TextMeshProUGUI grainCounter;
+    [SerializeField] private TextMeshProUGUI stoneCounter;
+
+    private string savePath;
+    
     private Dictionary<ResourceType, int> resourceCurrencies = new Dictionary<ResourceType, int>()
     {
         { ResourceType.Wood, 0 },
         { ResourceType.Grain, 0 },
         { ResourceType.Stone, 0 }
     };
-    
-    void Start(){
-        //initializing with editor given no..
+
+    void Start()
+{
+    savePath = Application.persistentDataPath + "/economyData.json";
+
+    if (File.Exists(savePath))
+    {
+        LoadEconomy(); // Load saved data if it exists
+    }
+    else
+    {
+        // If no saved data, initialize with default values from Inspector
         resourceCurrencies[ResourceType.Wood] = deltaWood;
         resourceCurrencies[ResourceType.Grain] = deltaGrain;
         resourceCurrencies[ResourceType.Stone] = deltaStone;
-        UpdateUICounter();
+        
+        SaveEconomy(); // Save initial values to prevent data loss
     }
-    
-    private void UpdateUICounter()
-{
-    // Update counter UI with formatted numbers
-    woodsCounter.text = "W: " + NumberFormatter.FormatNumber(resourceCurrencies[ResourceType.Wood]);
-    grainCounter.text = "G: " + NumberFormatter.FormatNumber(resourceCurrencies[ResourceType.Grain]);
-    stoneCounter.text = "S: " + NumberFormatter.FormatNumber(resourceCurrencies[ResourceType.Stone]);
+
+    UpdateUICounter();
 }
+
+
+    private void UpdateUICounter()
+    {
+        woodsCounter.text = "W: " + NumberFormatter.FormatNumber(resourceCurrencies[ResourceType.Wood]);
+        grainCounter.text = "G: " + NumberFormatter.FormatNumber(resourceCurrencies[ResourceType.Grain]);
+        stoneCounter.text = "S: " + NumberFormatter.FormatNumber(resourceCurrencies[ResourceType.Stone]);
+    }
 
     public static class NumberFormatter
-{
-    public static string FormatNumber(float num)
     {
-        if (num >= 1_000_000_000) // Billion
-            return (num / 1_000_000_000f).ToString("0.##") + "B";
-        else if (num >= 1_000_000) // Million
-            return (num / 1_000_000f).ToString("0.##") + "M";
-        else if (num >= 1_000) // Thousand
-            return (num / 1_000f).ToString("0.#") + "K";
-        else
-            return num.ToString("0"); // Normal number
+        public static string FormatNumber(float num)
+        {
+            if (num >= 1_000_000_000) return (num / 1_000_000_000f).ToString("0.##") + "B";
+            else if (num >= 1_000_000) return (num / 1_000_000f).ToString("0.##") + "M";
+            else if (num >= 1_000) return (num / 1_000f).ToString("0.#") + "K";
+            else return num.ToString("0");
+        }
     }
-}
-
-
 
     public void AddResource(ResourceType type, int amount)
     {
-        //collection of resource by farms.
-        switch (type)
-        {
-            case ResourceType.Wood:
-                resourceCurrencies[ResourceType.Wood] += amount;
-                break;
-            case ResourceType.Grain:
-                resourceCurrencies[ResourceType.Grain] += amount;
-                break;
-            case ResourceType.Stone:
-                resourceCurrencies[ResourceType.Stone] += amount;
-                break;
-        }
+        resourceCurrencies[type] += amount;
+        UpdateUICounter();
+        SaveEconomy();
     }
-    public void CollectMinedResource(int[] resource){
+
+    public void CollectMinedResource(int[] resource)
+    {
         resourceCurrencies[ResourceType.Wood] += resource[0];
         resourceCurrencies[ResourceType.Grain] += resource[1];
         resourceCurrencies[ResourceType.Stone] += resource[2];
-        Debug.Log("Resource added to the treasure {w,g,s}: "+resource[0]+resource[1]+resource[2]);
-        UpdateUICounter();   
 
+        Debug.Log($"Resources Collected: W={resource[0]}, G={resource[1]}, S={resource[2]}");
+        UpdateUICounter();
+        SaveEconomy();
     }
 
-    public void CollectingAllresourceAmount(ResourceType resourceTypeToCollect){
-        //this will be called by TriggerCollectionOfresourceAmount in farm (indirectly by ui icon)
-        
+    public void CollectingAllresourceAmount(ResourceType resourceTypeToCollect)
+    {
         Farm[] allFarms = FindObjectsOfType<Farm>();
         foreach (Farm farm in allFarms)
         {
@@ -88,28 +89,76 @@ public class CurrencyManager : MonoBehaviour
                 int collected = farm.ReturnResourceAmount();
                 AddResource(farm.resourceType, collected);
             }
-          
-        }        
-            UpdateUICounter();   
-    }
-     public Dictionary<ResourceType, int> ReturnAllResources()
-    {
-        return new Dictionary<ResourceType, int>(resourceCurrencies); // Return a copy of the dictionary
+        }
+        UpdateUICounter();
+        SaveEconomy();
     }
 
-    public void SpendBuildingCost(int woodCost,int grainCost,int stoneCost){
-        //spending resources 
-        //this needs to be changed.
+    public Dictionary<ResourceType, int> ReturnAllResources()
+    {
+        return new Dictionary<ResourceType, int>(resourceCurrencies);
+    }
+
+    public void SpendBuildingCost(int woodCost, int grainCost, int stoneCost)
+    {
         resourceCurrencies[ResourceType.Wood] -= woodCost;
         resourceCurrencies[ResourceType.Grain] -= grainCost;
-        resourceCurrencies[ResourceType.Stone] -= stoneCost;     
+        resourceCurrencies[ResourceType.Stone] -= stoneCost;
+
         UpdateUICounter();
+        SaveEconomy();
     }
-   
+
+    // 💾 Saving & Loading Economy Data
+
+    private void SaveEconomy()
+    {
+        EconomyData data = new EconomyData
+        {
+            stone = resourceCurrencies[ResourceType.Stone],
+            grain = resourceCurrencies[ResourceType.Grain],
+            wood = resourceCurrencies[ResourceType.Wood]
+        };
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(savePath, json);
+        Debug.Log("Economy Saved: " + json);
+    }
+
+    private void LoadEconomy()
+    {
+        if (File.Exists(savePath))
+        {
+            string json = File.ReadAllText(savePath);
+            EconomyData data = JsonUtility.FromJson<EconomyData>(json);
+
+            resourceCurrencies[ResourceType.Stone] = data.stone;
+            resourceCurrencies[ResourceType.Grain] = data.grain;
+            resourceCurrencies[ResourceType.Wood] = data.wood;
+
+            Debug.Log($"Economy Loaded: Stone={data.stone}, Grain={data.grain}, Wood={data.wood}");
+        }
+        else
+        {
+            Debug.Log("No economy save file found, using default values.");
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        SaveEconomy(); // Save data when quitting the game
+    }
 }
 
+[System.Serializable]
+public class EconomyData
+{
+    public int stone;
+    public int grain;
+    public int wood;
+}
 
-//for all resources
+// Resource Type Enum
 public enum ResourceType
 {
     Wood,
